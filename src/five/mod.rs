0768451ebee46fec0dd7c2c4563a2d8b27a6492c
Vignesh Kuppusamy <viggykuppu@gmail.com>
2024-12-05 12:38:35 -0500

    code cleanup

--- src/five/mod.rs
+++ src/five/mod.rs
@@ -5,93 +5,72 @@ use regex::Regex;
 #[aocd(2024, 5)]
 pub fn one() {
     let input = input!();
-    let mut after_map: HashMap<u32, HashSet<u32>> = HashMap::new();
-    let ordering_regex = Regex::new(r"(\d+)\|(\d+)").unwrap();
-    let sum: u32 = input.lines().enumerate().map(|(i, line)| {
-        // build ordering
-        if line.contains('|') {
-            let caps = ordering_regex.captures(line).unwrap();
-            let x = caps.get(1).unwrap().as_str().parse::<u32>().unwrap();
-            let y = caps.get(2).unwrap().as_str().parse::<u32>().unwrap();
-            if !after_map.contains_key(&x) {
-                after_map.insert(x, HashSet::new());
-            }
-            after_map.get_mut(&x).unwrap().insert(y);
-        } else if line.contains(',') {
-            let nums: Vec<u32> = line.split(',').map(|d| d.parse::<u32>().unwrap()).collect();
-            let mut seen_nums:HashSet<u32> = HashSet::new();
-            let mut valid = true;
-            for num in nums.iter() {
-                valid = seen_nums.iter().all(|seen_num| {
-                    if after_map.contains_key(&num) {
-                        !after_map.get(&num).unwrap().contains(seen_num)
-                    } else {
-                        true
-                    }
-                });
-                if !valid {
-                    break;
-                }
-                seen_nums.insert(*num);
-            }
-            if valid {
-                let middle_num = nums.get(nums.len()/2).unwrap();
-                return *middle_num;
-            }
-        }
-        return 0;
+    let after_map = parse_rules(&input);
+    let sum: u32 = input.lines().filter(|line| line.contains(',') && is_valid_page(line, &after_map)).map(|line| {
+        let nums: Vec<u32> = line.split(',').map(|d| d.parse::<u32>().unwrap()).collect();
+        let middle_num = nums.get(nums.len()/2).unwrap();
+        return *middle_num;
     }).sum();
-
     submit!(1, sum);
 }
 
 #[aocd(2024, 5)]
 pub fn two() {
     let input = input!();
+    let after_map = parse_rules(&input);
+    let sum: u32 = input.lines().filter(|line| line.contains(',') && !is_valid_page(line, &after_map)).map(|line| {
+        let nums: Vec<u32> = line.split(',').map(|d| d.parse::<u32>().unwrap()).collect();
+        let mut bad_page = nums.clone();
+        bad_page.sort_by(|a, b| {
+            if after_map.contains_key(a) {
+                if after_map[a].contains(b) {
+                    return Ordering::Less;
+                }
+            } else if after_map.contains_key(b) {
+                if after_map[b].contains(a) {
+                    return Ordering::Greater;
+                }
+            }
+            return Ordering::Equal;
+        });
+        return *bad_page.get(bad_page.len()/2).unwrap();
+    }).sum();
+
+    submit!(2, sum);
+}
+
+fn parse_rules(input: &str) -> HashMap<u32, HashSet<u32>> {
     let mut after_map: HashMap<u32, HashSet<u32>> = HashMap::new();
     let ordering_regex = Regex::new(r"(\d+)\|(\d+)").unwrap();
-    let sum: u32 = input.lines().enumerate().map(|(i, line)| {
+    input.lines().for_each(|line| {
         if line.contains('|') {
             let caps = ordering_regex.captures(line).unwrap();
             let x = caps.get(1).unwrap().as_str().parse::<u32>().unwrap();
             let y = caps.get(2).unwrap().as_str().parse::<u32>().unwrap();
-            if !after_map.contains_key(&x) {
-                after_map.insert(x, HashSet::new());
-            }
-            after_map.get_mut(&x).unwrap().insert(y);
-        } else if line.contains(',') {
-            let nums: Vec<u32> = line.split(',').map(|d| d.parse::<u32>().unwrap()).collect();
-            let mut seen_nums:HashSet<u32> = HashSet::new();
-
-            for num in nums.iter() {
-                let valid = seen_nums.iter().all(|seen_num| {
-                    if after_map.contains_key(&num) {
-                        !after_map.get(&num).unwrap().contains(seen_num)
-                    } else {
-                        true
-                    }
-                });
-                if !valid {
-                    let mut bad_page = nums.clone();
-                    bad_page.sort_by(|a, b| {
-                        if after_map.contains_key(a) {
-                            if after_map.get(a).unwrap().contains(b) {
-                                return Ordering::Less;
-                            }
-                        } else if after_map.contains_key(b) {
-                            if after_map.get(b).unwrap().contains(a) {
-                                return Ordering::Greater;
-                            }
-                        }
-                        return Ordering::Equal;
-                    });
-                    return *bad_page.get(bad_page.len()/2).unwrap();
-                }
-                seen_nums.insert(*num);
-            }
+            after_map.entry(x).or_default().insert(y);
         }
-        0
-    }).sum();
+    });
+    return after_map;
+}
 
-    submit!(2, sum);
+fn is_valid_page(line: &str, after_map: &HashMap<u32,HashSet<u32>>) -> bool {
+    if !line.contains(',') {
+        return false;
+    }
+    let nums: Vec<u32> = line.split(',').map(|d| d.parse::<u32>().unwrap()).collect();
+    let mut seen_nums:HashSet<u32> = HashSet::new();
+    for num in nums.iter() {
+        let valid = seen_nums.iter().all(|seen_num| {
+            if after_map.contains_key(&num) {
+                !after_map.get(&num).unwrap().contains(seen_num)
+            } else {
+                true
+            }
+        });
+        if !valid {
+            return false;
+        }
+        seen_nums.insert(*num);
+    }
+    true
 }
