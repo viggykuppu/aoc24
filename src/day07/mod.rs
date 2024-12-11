30cdbf2916229ce3be1eaf7afdc916f188cc6b34
Vignesh Kuppusamy <viggykuppu@gmail.com>
2024-12-11 13:19:29 -0500

    Updated day 7 solution

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -2,80 +2,66 @@ use aocd::*;
 use radix_fmt::radix_3;
 use regex::Regex;
 use rayon::prelude::*;
+use itertools::Itertools;
 
 #[aocd(2024, 7)]
 pub fn one() {
     let input = input!();
     let line_regex = Regex::new(r"(\d+): (.*)").unwrap();
+    let operators = ['+', '*'];
     let total_valid_lines: u64 = input.lines().collect::<Vec<_>>().par_iter().map(|line| {
         let caps: Vec<_> = line_regex.captures_iter(line).collect();
         let total = caps.first().unwrap().get(1).unwrap().as_str().parse::<u64>().unwrap();
         let nums: Vec<_> = caps.first().unwrap().get(2).unwrap().as_str().split(' ').map(|n| n.parse::<u64>().unwrap()).collect();
-        let total_combos = 2_u32.pow(nums.len() as u32 - 1);
-        for i in 0..total_combos {
-            let result = nums.iter().enumerate().fold(0, |acc, (j, next)| {
-                if j == 0 {
-                    return acc + next;
-                }
-                let operation_bit = (i >> (j-1)) & 1;
-                if operation_bit == 0 {
-                    acc + next
-                } else {
-                    acc * next
-                }
-            });
-            if result == total {
-                return total;
-            }
+        if check_if_valid(total, &nums, &operators) {
+            return total;
         }
         0
     }).sum();
     submit!(1, total_valid_lines);
 }
 
-fn is_valid(total: u64, nums: Vec<u64>, ops: &[char]) -> u64 {
-    let mut t = 0;
-    for op in ops {
-        if op == &'+' {
-            t = total + nums[0];
-        } else if op == &'*' {
-            t = total * nums[0];
-        }
-    }
-    t
-}
-
 #[aocd(2024, 7)]
 pub fn two() {
     let input = input!();
     let line_regex = Regex::new(r"(\d+): (.*)").unwrap();
-    let total_valid_lines = input.lines().collect::<Vec<_>>().par_iter().map(|line| {
+    let old_operators = ['+', '*'];
+    let operators = ['+', '*', '|'];
+    let total_valid_lines: u64 = input.lines().collect::<Vec<_>>().par_iter().map(|line| {
         let caps: Vec<_> = line_regex.captures_iter(line).collect();
         let total = caps.first().unwrap().get(1).unwrap().as_str().parse::<u64>().unwrap();
         let nums: Vec<_> = caps.first().unwrap().get(2).unwrap().as_str().split(' ').map(|n| n.parse::<u64>().unwrap()).collect();
-        let total_combos = 3_u32.pow(nums.len() as u32 - 1);
-        for i in 0..total_combos {
-            let result = nums.iter().enumerate().fold(0, |acc, (j, next)| {
-                if j == 0 {
-                    return acc + next;
-                }
-                let mut i_base_3: Vec<_> = radix_3(i).to_string().chars().collect();
-                i_base_3.reverse();
-                let operation_bit = i_base_3.get((j-1) as usize).unwrap_or(&'0');
-                if operation_bit == &'0' {
-                    acc + next
-                } else if operation_bit == &'1' {
-                    acc * next
-                } else {
-                    let concatenated = acc.to_string() + &next.to_string();
-                    concatenated.parse::<u64>().unwrap()
-                }
-            });
-            if result == total {
-                return total;
-            }
+        if check_if_valid(total, &nums, &old_operators) {
+            return total;
+        } else if check_if_valid(total, &nums, &operators) {
+            return total;
         }
         0
-    }).sum::<u64>();
+    }).sum();
     submit!(2, total_valid_lines);
+}
+
+fn check_if_valid(total: u64, nums: &[u64], operators: &[char]) -> bool {
+    let combos: Vec<_> = itertools::repeat_n(operators, nums.len()-1).multi_cartesian_product().collect();
+    for combo in combos.iter() {
+        let result = nums.iter().enumerate().fold(0, |acc, (i, next)| {
+            if i == 0 {
+                return *next;
+            }
+            let op = *combo[i-1];
+            if op == '+' {
+                return acc + next;
+            } else if op == '*' {
+                return acc * next;
+            } else if op == '|' {
+                let concatenated = acc.to_string() + &next.to_string();
+                return concatenated.parse::<u64>().unwrap();
+            }
+            0
+        });
+        if result == total {
+            return true;
+        }
+    }
+    false
 }
