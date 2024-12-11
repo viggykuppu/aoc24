d8668859f2ca1c4bed6c92061b4092a44cc51bcb
Vignesh Kuppusamy <viggykuppu@gmail.com>
2024-12-11 14:34:03 -0500

    Cleaning up day 9, inspiration from joey

--- src/day09/mod.rs
+++ src/day09/mod.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 
 use aocd::*;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 #[aocd(2024, 9)]
 pub fn one() {
@@ -57,154 +58,42 @@ pub fn one() {
 #[aocd(2024, 9)]
 pub fn two() {
     let input = input!();
-    let mut index: u64 = 0;
-    let mut disk: Vec<Option<u64>> = Vec::new();
+    let mut block_id: u64 = 0;
+    let mut disk: Vec<(Option<u64>, u32, u32)> = Vec::new();
+    let mut free_space: Vec<(u32, u32)> = Vec::new();
+    let mut location = 0;
     input.chars().enumerate().for_each(|(i, char)| {
         let num_blocks = char.to_digit(10).unwrap();
-        let new_block = if i %2 == 0 {
-            // file
-            index += 1;
-            Some(index-1)
+        if i %2 == 0 {
+            disk.push((Some(block_id), num_blocks, location));
+            block_id += 1;
         } else {
-            // free space
-            None
+            disk.push((None, num_blocks, location));
+            free_space.push((location, num_blocks));
         };
-        for _ in 0..num_blocks {
-            disk.push(new_block);
-        }
+        location += num_blocks;
     });
-
-    let mut disk_reverse = disk.clone();
-    disk_reverse.reverse();
-    let mut condensed_disk: Vec<Option<u64>> = Vec::new();
-    let mut i = 0;
-    let mut moved_blocks: HashSet<u64> = HashSet::new();
-    while condensed_disk.len() < disk.len() {
-        if condensed_disk.len() == disk.len() {
-            break;
-        }
-        let block = disk[i];
-        let mut full_block = get_full_block(&disk, i);
-        if let Some(v) = block {
-            // this is an already placed block so let's add it to our list unless we've already moved it
-            i += full_block.len();
-            if moved_blocks.contains(&v) {
-                for _ in 0..full_block.len() {
-                    condensed_disk.push(None);
-                }
-            } else {
-                condensed_disk.append(&mut full_block);
-            }
-        } else {
-            // free block
-            let mut j = 0;
-            let mut added_block = false;
-            while j < disk_reverse.len() {
-                if disk.len() - (j) < i {
+    for block in disk.iter_mut().rev() {
+        if block.0.is_some() {
+            for free in free_space.iter_mut() {
+                if block.1 <= free.1 && block.2 > free.0 {
+                    block.2 = free.0;
+                    free.1 = free.1 - block.1;
+                    free.0 = free.0 + block.1;
                     break;
                 }
-                let mut full_reverse_block = get_full_block(&disk_reverse, j);
-                let full_reverse_block_length = full_reverse_block.len();
-                if let Some(v) = disk_reverse[j] {
-                    if !moved_blocks.contains(&v) && full_reverse_block.len() <= full_block.len() {
-                        moved_blocks.insert(v);
-                        i += full_reverse_block.len();
-                        condensed_disk.append(&mut full_reverse_block);
-                        added_block = true;
-                        break;
-                    }
-                }
-                j += full_reverse_block_length;
-            }
-            if !added_block {
-                i += full_block.len();
-                for _ in 0..full_block.len() {
-                    condensed_disk.push(None);
-                }
             }
         }
     }
-    let checksum: u64 = condensed_disk.iter().enumerate().map(|(i, n)| {
-        (i as u64)*(n.unwrap_or(0))
-    }).sum();
-
-    submit!(2, checksum);
-}
-
-#[aocd(2024, 9)]
-pub fn two_clean() {
-    let input = input!();
-    let mut index: u64 = 0;
-    let mut disk: Vec<Option<u64>> = Vec::new();
-    input.chars().enumerate().for_each(|(i, char)| {
-        let num_blocks = char.to_digit(10).unwrap();
-        let new_block = if i %2 == 0 {
-            // file
-            index += 1;
-            Some(index-1)
-        } else {
-            // free space
-            None
-        };
-        for _ in 0..num_blocks {
-            disk.push(new_block);
-        }
-    });
-
-    let mut disk_reverse = disk.clone();
-    disk_reverse.reverse();
-
-    let mut i = 0;
-    while i < disk_reverse.len() {
-        let full_block = get_full_block(&disk_reverse, i);
-        let original_index = disk.len() - (i+full_block.len());
-        if let Some(index) = find_space(&disk, full_block.len()) {
-            if index < original_index {
-                for j in 0..full_block.len() {
-                    disk.swap(index + j, original_index + j);
-                }
+    let checksum: u64 = disk.par_iter().map(|block| {
+        if let Some(v) = block.0 {
+            let mut total = 0;
+            for i in block.2..(block.2 + block.1) {
+                total += (i as u64)*v;
             }
+            return total;
         }
-        pretty_print_disk(&disk);
-        i += full_block.len();
-    }
-    let checksum: u64 = disk.iter().enumerate().map(|(i, n)| {
-        (i as u64)*(n.unwrap_or(0))
+        0
     }).sum();
     submit!(2, checksum);
-}
-
-fn pretty_print_disk(disk: &Vec<Option<u64>>) {
-    for x in disk {
-        if let Some(a) = x {
-            print!("{a}");
-        } else {
-            print!(".");
-        }
-    }
-    println!();
-}
-
-fn find_space(disk: &[Option<u64>], space: usize) -> Option<usize> {
-    let mut i = 0;
-    while i < disk.len() {
-        let block_start = disk[i];
-        let full_block = get_full_block(disk, i);
-        if block_start.is_none() && full_block.len() >= space {
-            return Some(i);
-        }
-        i += full_block.len();
-    }
-    None
-}
-
-fn get_full_block(disk: &[Option<u64>], index: usize) -> Vec<Option<u64>> {
-    let mut full_block: Vec<Option<u64>> = Vec::new();
-    let start = disk[index];
-    let mut i = index;
-    while (0..disk.len()).contains(&i) && disk[i] == start {
-        full_block.push(disk[i]);
-        i += 1;
-    }
-    full_block
 }
--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,11 @@ mod day11;
 
 fn main() {
     let mut now = Instant::now();
-    day07::one();
+    day09::one();
     let part_one_time = now.elapsed().as_millis();
     println!("Part 1 solved in {part_one_time} ms");
     now = Instant::now();
-    day07::two();
+    day09::two();
     let part_two_time = now.elapsed().as_millis();
     println!("Part 2 solved in {part_two_time} ms");
 }
