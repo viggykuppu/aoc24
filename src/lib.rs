b6ee9341ad3eda3cca860a6b6fd4e1b138bd70bd
Vignesh Kuppusamy <viggykuppu@gmail.com>
2024-12-21 18:19:47 -0500

    Day 21 progress

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@ pub mod day17;
 pub mod day18;
 pub mod day19;
 pub mod day20;
+pub mod day21;
 
 pub fn read_input(filename: &str) -> String {
     let mut file = File::open(filename).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use aoc24::*;
 
 fn main() {
     let mut now = Instant::now();
-    day20::one();
+    day21::one();
     let part_one_time = now.elapsed().as_millis();
     println!("Part 1 solved in {part_one_time} ms");
     now = Instant::now();
-    day20::two();
+    day21::two();
     let part_two_time = now.elapsed().as_millis();
     println!("Part 2 solved in {part_two_time} ms");
 }
