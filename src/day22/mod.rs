5fb9cace8394c6146a7a1f6b67eb975e2892c8b1
Vignesh <viggykuppu@gmail.com>
2024-12-22 22:47:37 -0500

    day 22 - sped up

--- src/day22/mod.rs
+++ src/day22/mod.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, usize};
+use std::{collections::{HashMap, HashSet}, usize};
 
 use aocd::*;
 use itertools::Itertools;
@@ -33,34 +33,31 @@ fn prune(a: i64) -> i64 {
 pub fn two() {
     let input = input!();
     let mut delta_maxes = HashMap::new();
-    input.lines().for_each(|line| {
+    let mut max_value = 0;
+    input.lines().enumerate().for_each(|(id, line)| {
+        let id = id as isize;
         let mut secret = line.parse::<i64>().unwrap();
-        let mut secrets = Vec::new();
-        for _ in 0..2000 {
+        let mut previous = 0;
+        let mut hash: u32 = 0;
+        for i in 0..2000 {
             secret = prune(mix(secret, secret * 64));
             secret = prune(mix(secret, secret / 32));
             secret = prune(mix(secret, secret * 2048));
-            secrets.push(secret % 10);
-        }
-        let deltas: Vec<_> = secrets.windows(2).map(|a| a[1] - a[0]).collect();
-        let mut maxes = HashMap::new();
-        deltas.windows(4).enumerate().for_each(|(i, delta)| {
-            let delta_string = String::from_iter(delta.iter().map(|d| d.to_string()));
-            let price = secrets[i + 4];
-            if maxes.get(&delta_string).is_none() {
-                maxes.insert(delta_string, price);
+            let price = (secret % 10) as i8;
+            let delta = price - previous;
+            hash = (hash << 8) | (delta as u32 & 0xff);
+            if i > 3 {
+                if price > 0 {
+                    let (last_inserted_id, max) = delta_maxes.entry(hash).or_insert((-1, 0));
+                    if *last_inserted_id != id {
+                        *max += price as u32;
+                        *last_inserted_id = id;
+                        max_value = max_value.max(*max);
+                    }
+                }
             }
-        });
-        for (key, value) in maxes.iter() {
-            let max = delta_maxes.entry(key.clone()).or_insert(0);
-            *max += value;
+            previous = price;
         }
     });
-    let mut max_value = 0;
-    for v in delta_maxes.values() {
-        if *v > max_value {
-            max_value = *v;
-        }
-    }
     submit!(2, max_value);
 }
